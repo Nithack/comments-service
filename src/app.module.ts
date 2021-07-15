aed0fd09c524ceda455b925ebd6189ae8847e683
Nithack <andreynithack@gmail.com>
2021-07-15 01:04:50 -0300

    feat: implement connection top app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
 import { CommentsModule } from './comments/comments.module';
 
 @Module({
-  imports: [CommentsModule],
+  imports: [TypeOrmModule.forRoot(), CommentsModule],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private connection: Connection) {}
+}
