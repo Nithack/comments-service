0dc4c47f56c5d1db40c45ada52b17e8857527fd2
Nithack <andreynithack@gmail.com>
2021-07-15 01:06:17 -0300

    refactor: finish comment implementation

--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -1,26 +1,74 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateCommentDto } from './dto/create-comment.dto';
+import { ResponseCommentDto } from './dto/response-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
+import { Comment } from './entities/comment.entity';
 
 @Injectable()
 export class CommentsService {
-  create(createCommentDto: CreateCommentDto) {
-    return 'This action adds a new comment';
+  constructor(
+    @InjectRepository(Comment)
+    private commentRepository: Repository<Comment>,
+  ) {}
+  async create(
+    createCommentDto: CreateCommentDto,
+  ): Promise<ResponseCommentDto> {
+    try {
+      const newComment = await this.commentRepository.save(createCommentDto);
+      return ResponseCommentDto.factory(true, newComment);
+    } catch (error) {
+      return ResponseCommentDto.factory(false, error.message);
+    }
   }
 
-  findAll() {
-    return `This action returns all comments`;
+  async findAll(): Promise<ResponseCommentDto> {
+    try {
+      const findComments = (await this.commentRepository.find()) || null;
+      if (!findComments)
+        return ResponseCommentDto.factory(false, 'no comments');
+      return ResponseCommentDto.factory(true, findComments);
+    } catch (error) {
+      return ResponseCommentDto.factory(false, error.message);
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} comment`;
+  async findOne(uuid: string): Promise<ResponseCommentDto> {
+    try {
+      const findComments =
+        (await this.commentRepository.find({ uuid })) || null;
+      if (!findComments)
+        return ResponseCommentDto.factory(false, 'comment not found');
+      return ResponseCommentDto.factory(true, findComments);
+    } catch (error) {
+      return ResponseCommentDto.factory(false, error.message);
+    }
   }
 
-  update(id: number, updateCommentDto: UpdateCommentDto) {
-    return `This action updates a #${id} comment`;
+  async update(
+    uuid: string,
+    updateCommentDto: UpdateCommentDto,
+  ): Promise<ResponseCommentDto> {
+    try {
+      const findComment =
+        (await this.commentRepository.findOne({ uuid })) || null;
+      if (!findComment) return ResponseCommentDto.factory(false, 'test');
+      Object.assign(findComment, updateCommentDto);
+      const saveComment = await this.commentRepository.save(findComment);
+      console.log(saveComment);
+      return ResponseCommentDto.factory(true, saveComment);
+    } catch (error) {
+      return ResponseCommentDto.factory(false, error.message);
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} comment`;
+  async remove(uuid: string): Promise<ResponseCommentDto> {
+    try {
+      await this.commentRepository.delete(uuid);
+      return ResponseCommentDto.factory(true, 'test');
+    } catch (error) {
+      return ResponseCommentDto.factory(false, error.message);
+    }
   }
 }
